082cf900ab9199a0983150c5500becb40b11b077
Eshaan Amin <64734948+eshaanganesh2@users.noreply.github.com>
2025-12-15 12:39:50 -0500

    Q1 HW

---
+++ week6/Q1.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* helper(TreeNode* root, TreeNode* p, TreeNode* q){
+        if(root==NULL){
+            return NULL;
+        }
+        if(root==p||root==q){
+            return root;
+        }
+        TreeNode*left=helper(root->left,p,q);
+        TreeNode*right=helper(root->right,p,q);
+        if(left&&right){
+            return root;
+        }
+        if(left){
+            return left;
+        }
+        if(right){
+            return right;
+        }
+        return NULL;
+    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        return helper(root,p,q);
+    }
+};
