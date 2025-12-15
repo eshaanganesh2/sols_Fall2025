fbc9106b7a5d9e96f6d49b7e8049409678a55c32
Eshaan Amin <64734948+eshaanganesh2@users.noreply.github.com>
2025-12-15 12:50:26 -0500

    Q3 HW

---
+++ week8/Q3.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n =nums.size();
+        int sum=0;
+        int result=INT_MIN;
+        for(int i=0;i<n;i++){
+            if(sum<0){
+                sum=0;
+            }
+            sum+=nums[i];
+            result=max(result,sum);
+        }
+        return result;
+    }
+};
